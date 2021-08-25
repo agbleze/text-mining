cbd573cacf32e7e7999eaf790c53b5f3229be275
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-08-25 23:07:26 +0200

    Update text-analysis.R

--- text-analysis.R
+++ text-analysis.R
@@ -1,4 +1,18 @@
 ### text files
+library(pdftools)
+library(magrittr)
+library(tidyverse)
+library(tidytext)
+library(dplyr)
+library(janeaustenr)
+library(stringr)
+library(tidyr)
+library(ggplot2)
+library(wordcloud)
+library(igraph)
+library(ggraph)
+library(tidygraph)
+
 
 file_vector <- list.files(path = "Data")
 file_vector %>% head()
@@ -7,4 +21,39 @@ grepl(".pdf", file_vector)
 ## select only pdf
 pdf_list <- file_vector[grepl("pdf", file_vector)]
 head(pdf_list)
-pdf_text("Data/BUSINESSCENTER.pdf")
+pdf_text("Data/BUSINESSCENTER.pdf")%>%
+  strsplit(split = "\n")
+
+corpus_raw <- data.frame("company" = c(), "text" = c())
+
+for (i in 1:length(pdf_list)){
+  print(i)
+  pdf_text(paste("data/", pdf_list[i], sep = ""))%>%
+    strsplit("\n") -> document_text
+  data.frame("company" = gsub(x = pdf_list[i], pattern = ".pdf", replacement = ""),
+             "text" = document_text, stringsAsFactors = FALSE) -> document
+  colnames(document) <- c("company", "text")
+  corpus_raw <- rbind(corpus_raw, document)
+}
+corpus_raw%>%head()
+
+### removing irrelevant information
+corpus_raw %>%
+  filter(!grepl("12.05.2017", text)) %>%
+  filter(!grepl("business profile", text)) %>%
+  filter(!grepl("comments", text)) %>%
+  filter(!grepl("1", text)) -> corpus
+
+corpus %>%
+  filter(!grepl(c("date of foundation"), text)) %>%
+  filter(!grepl(c("industry"), text)) %>%
+  filter(!grepl(c("share holders"), text)) -> comments
+
+corpus %>%
+  filter(grepl(("date of foundation"), text) | grepl(("industry"), text) | grepl(("share holders"), text)) -> information
+
+
+## tokeninzing
+comments %>%
+  unnest_tokens(word, text) -> comments_tidy
+
