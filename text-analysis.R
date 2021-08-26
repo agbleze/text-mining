02eba58e40ef29e4a92bfe6054dd74753369f3bd
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-08-26 20:59:58 +0200

    Create text-analysis.R

--- text-analysis.R
+++ text-analysis.R
@@ -72,3 +72,89 @@ ggplot(comments_sentiment, aes(x = sentiment)) + geom_histogram()
 comments_tidy %>%
   count(word) %>%
   with(wordcloud(word, n))
+
+
+
+####### remove stop_words  and plot wordcloud ############
+comments_tidy %>%
+  filter(!word %in% stop_words$word) %>%
+  count(word) %>%
+  with(wordcloud(word, n))
+
+
+#### find context in text analysis with n-grams  ######
+comments %>%
+  unnest_tokens(bigram, text, token = "ngrams", n = 2) -> bigram_comments
+
+## FILTER bigram from stop_words and count them
+bigram_comments %>%
+  separate(bigram, c("word1", "word2"), sep = " ")%>%
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word) %>%
+  count(word1, word2, sort = TRUE)
+
+###### trigrams
+comments %>%
+  unnest_tokens(trigram, text, token = "ngrams", n = 3) -> trigram_comments
+
+trigram_comments %>%
+  separate(trigram, c("word1", "word2", "word3"), sep = " ")%>%
+  filter(!word1 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word) %>%
+  filter(!word2 %in% stop_words$word) %>%
+  count(word1, word2, word3, sort = TRUE) -> trigram_words
+
+#############  network analysis  ##
+information %>%
+  filter(grepl("industry", text))
+
+### clean in text col to have industry
+information %>%
+  filter(grepl("industry", text)) %>%
+  mutate(industry = gsub("industry: ", "", text)) -> industries
+
+## plot results of industries
+industries %>%
+  count(industry) %>%
+  filter(n > 1) %>%
+  ggplot(aes(x = industry, y = n)) +
+  geom_bar(stat = "identity") +
+  coord_flip()
+
+#### filtering shareholder info
+information %>%
+  filter(grepl("share holders", text)) %>%
+  mutate(shareholders = gsub("share holders: ", "", text))
+
+### separate each shareholder into diff col 
+information %>%
+  filter(grepl("share holders", text)) %>%
+  mutate(shareholders = gsub("share holders: ", "", text)) %>%
+  separate(col = shareholders, into = c("first", "second", "third"), sep = ";") %>%
+  ## gather 
+  gather(key = "number", value = "shareholder", -company, -text) %>%
+  ## filter those not shareholders
+  filter(!is.na(shareholder)) %>%
+  select(company, shareholder) -> shareholders
+
+
+#### visualizing the graph
+# obtain edge list from data
+graph_from_data_frame(shareholders) -> graph_object
+graph_object%>%
+  ggraph() +
+  geom_edge_link(alpha = .2) +
+  geom_node_point(alpha = .3) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1, check_overlap = TRUE) + theme_graph()
+
+## cal degree for nodes
+deg <- degree(graph_object, mode = "all")
+# take node vertice of graph object and create attribute size and assign 3 time of the degree
+V(graph_object)$size <- deg*3
+
+
+#plot the degree of graph
+set.seed(30)
+graph_object %>%
+  ggraph() + geom_edge_link(alpha = .2) + geom_node_point(aes(size = size), alpha = .3) + 
+  geom_node_text(aes(label = name, size = size), vjust = 1, hjust = 1, check_overlap = FALSE) + theme_graph()
