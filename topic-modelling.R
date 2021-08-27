02b8fa2e6d91689b73241ae2098a35e8e343a3fc
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-08-27 23:53:03 +0200

    Update topic-modelling.R

--- topic-modelling.R
+++ topic-modelling.R
@@ -1,2 +1,10 @@
 ### topic modelling
 ## Latent Dirichlet Allocation (LDA)
+library(topicmodels)
+library(tidytext)
+data("AssociatedPress")
+AssociatedPress
+
+#LDA topic model with two topics
+ap_lda <- LDA(AssociatedPress, k = 2, control = list(seed = 1234))
+ap_lda
