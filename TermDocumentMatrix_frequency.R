0bc30a7f94ad91430f1c32459b2d2bee40ec5259
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-08-30 22:45:16 +0200

    Add files via upload

---
+++ TermDocumentMatrix_frequency.R
@@ -0,0 +1,79 @@
+######## TEXT Mining
+##### TermDocumentMatrix (TDM) frequency finding
+
+##set options
+options(stringsAsFactors = FALSE)
+Sys.setlocale("LC_ALL", "C")
+
+## load libraries
+library(stringi)
+library(stringr)
+library(qdap)
+library(tm)
+
+library(readr)
+text.df <- read_csv("/Volumes/SAMSUNG/text_mining-master 2/oct_delta.csv")
+
+######### tweeters   
+tweets <- data.frame(doc_id = seq(1:nrow(text.df)), text = text.df$text)
+
+# returns NA instead of tolower error
+tryTolower <- function(x){
+  # return NA when there is an error
+  y = NA
+  # tryCatch error
+  try_error <- tryCatch(tolower(x), error = function(e) e)
+  # if not an error
+  if (!inherits(try_error, "error"))
+    y = tolower(x)
+  return(y)
+}
+
+custom.stopwords <- c(stopwords('english'), 'lol', 'smh', 'delta')
+
+## customized function for transformation for text minining
+clean.corpus <- function(corpus){
+  corpus <- tm_map(corpus, content_transformer(tryTolower))
+  corpus <- tm_map(corpus, removeWords, custom.stopwords)
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, stripWhitespace)
+  corpus <- tm_map(corpus, removeNumbers)
+  return(corpus)
+}
+
+# create volatile corpus
+corpus <- VCorpus(DataframeSource(tweets)) 
+corpus <- clean.corpus(corpus)
+as.list(corpus)
+
+tm.definition <- 'Txt mining is the process of distilling actionable insyghts from text.'
+which_misspelled(tm.definition)
+check_spelling_interactive(tm.definition)
+
+## custome function to replace misspelled words
+fix.text <- function(myStr){
+  check <- check_spelling(myStr)
+  splitted <- strsplit(myStr, split = ' ')
+  for ( i in 1:length(check$row)) {
+    splitted[[check$row[i]]][as.numeric(check$word.no[i])] = check$suggestion[i]
+  }
+  df <- unlist(lapply(splitted, function(x) paste(x, collapse = ' ')))
+  return(df)
+}
+
+fix.text(tm.definition)
+
+## convert to TermDocumentMatrix
+tdm <- TermDocumentMatrix(corpus, control = list(weighting = weightTf))
+tdm.tweets.m <- as.matrix(tdm)
+
+# check dimension
+dim(tdm.tweets.m)
+## check result of some rows and columns
+tdm.tweets.m[2250:2255, 1340:1342]
+
+# find frequency of the terms
+term.freq <- rowSums(tdm.tweets.m)
+freq.df <- data.frame(word = names(term.freq), frequency = term.freq)
+freq.df <- freq.df[order(freq.df[,2], decreasing = T),]
+freq.df[1:10,]
